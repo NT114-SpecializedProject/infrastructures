8ae236136d210254a6227d54c4816a00ccb23359
datnguyen101004 <datnguyen101004@gmail.com>
2025-06-28 19:19:44 +0700

    update

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-southeast-1"
 }
 resource "aws_eks_cluster" "cluster" {
   name     = var.cluster_name
-  role_arn = var.cluster_iam_role_arn 
+  role_arn = var.cluster_iam_role_arn
 
   vpc_config {
     subnet_ids = var.subnet_ids
@@ -15,9 +15,9 @@ resource "aws_eks_cluster" "cluster" {
 resource "aws_eks_node_group" "node_group" {
   cluster_name    = var.cluster_name
   node_group_name = var.node_group_name
-  node_role_arn   = var.node_iam_role_arn 
+  node_role_arn   = var.node_iam_role_arn
   subnet_ids      = var.subnet_ids
-  instance_types   =var.instance_type
+  instance_types  = var.instance_type
   scaling_config {
     desired_size = 2
     max_size     = 4
@@ -34,28 +34,28 @@ resource "aws_eks_node_group" "node_group" {
 resource "aws_eks_addon" "coredns" {
   cluster_name = var.cluster_name
   addon_name   = "coredns"
- 
-  depends_on   = [aws_eks_cluster.cluster]
+
+  depends_on = [aws_eks_cluster.cluster]
 }
 
 resource "aws_eks_addon" "kube_proxy" {
   cluster_name = var.cluster_name
   addon_name   = "kube-proxy"
- 
-  depends_on   = [aws_eks_cluster.cluster]
+
+  depends_on = [aws_eks_cluster.cluster]
 }
 
 resource "aws_eks_addon" "vpc_cni" {
   cluster_name = var.cluster_name
   addon_name   = "vpc-cni"
 
-  depends_on   = [aws_eks_cluster.cluster]
+  depends_on = [aws_eks_cluster.cluster]
 }
 
 resource "aws_eks_addon" "eks_pod_identity_agent" {
   cluster_name = var.cluster_name
   addon_name   = "eks-pod-identity-agent"
- 
-  depends_on   = [aws_eks_cluster.cluster]
+
+  depends_on = [aws_eks_cluster.cluster]
 }
 
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,9 +1,9 @@
 # VPC vars
-name                = "eks"
-vpc_cidr            = "10.0.0.0/16"
-public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
+name                 = "eks"
+vpc_cidr             = "10.0.0.0/16"
+public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
 private_subnet_cidrs = ["10.0.11.0/24", "10.0.12.0/24"]
-availability_zones  = ["us-east-1a", "us-east-1b"]
+availability_zones   = ["ap-southeast-1a", "ap-southeast-1b"]
 
 # EKS vars
 cluster_name         = "cluster"
